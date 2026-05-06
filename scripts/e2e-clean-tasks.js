3bb5d867f2b6e042e70824dcd5457db995363470
Qipeng Gong <gongqipeng@umu.com>
2026-05-06 13:17:30 +0800

    chore: extend e2e-clean-tasks to also clear occurrences

--- scripts/e2e-clean-tasks.js
+++ scripts/e2e-clean-tasks.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
-// Cleanup script for E2E tests: removes all tasks for e2e_alice
+// Cleanup script for E2E tests: removes all tasks (and occurrences) for e2e_alice
 const { PrismaClient } = require("@prisma/client");
 const p = new PrismaClient();
 (async () => {
+  // Occurrences must be deleted before tasks (FK constraint)
+  await p.occurrence.deleteMany({ where: { user: { username: "e2e_alice" } } });
   await p.task.deleteMany({ where: { user: { username: "e2e_alice" } } });
   await p.$disconnect();
 })();
